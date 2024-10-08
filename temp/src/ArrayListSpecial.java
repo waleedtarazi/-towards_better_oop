11dbda5239cbb8eb93938c20b28a8e64274aa0e0
waleedtarazi <waleedaltarazy@gmail.com>
2024-10-08 11:52:42 +0300

    add/ arrayList add function

--- temp/src/ArrayListSpecial.java
+++ temp/src/ArrayListSpecial.java
@@ -1,10 +1,32 @@
 public class ArrayListSpecial {
     int length;
+    int currentIndex;
     int[] innerItems;
     public ArrayListSpecial(int length){
         innerItems = new int[length];
         this.length = length;
+        currentIndex = 0;
     }
 
+
+    private int[] moveItems(int[] currentItems, int size){
+        int[] temp = new int[size];
+        for(int i = 0; i<currentItems.length;i++){
+            temp[i] = currentItems[i];
+        }
+        return temp;
+    }
+
+    public void addItem(int item){
+        if(currentIndex<length){
+            innerItems[++currentIndex] = item;
+        } else if (currentIndex==length) {
+            length = length + length/2;
+            currentIndex = length;
+            innerItems = moveItems(innerItems,length);
+            innerItems[currentIndex++] = item;
+        }
+
+    }
 
 }
