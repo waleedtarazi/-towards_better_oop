4b9c91ce1a8ca57f1d12b5ea654441ec8e590be5
waleedtarazi <waleedaltarazy@gmail.com>
2024-10-08 11:42:14 +0300

    creating blueprint of arraylist

---
+++ temp/src/ArrayListSpecial.java
@@ -0,0 +1,10 @@
+public class ArrayListSpecial {
+    int length;
+    int[] innerItems;
+    public ArrayListSpecial(int length){
+        innerItems = new int[length];
+        this.length = length;
+    }
+
+
+}
